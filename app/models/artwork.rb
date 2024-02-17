0926406aab12e6dfa4897b2e164fac4ec24d1817
Ishita Sharma <isharma2@academic.rrc.ca>
2024-02-17 01:24:39 -0600

    Added validations to Artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,10 @@
 class Artwork < ApplicationRecord
+  validates :title, presence: true
+  validates :classification, presence: true
+  validates :begin_year, presence: true
+  validates :end_year, presence: true
+  validates :medium, presence: true
+  validates :provenance_text, presence: true
+  validates :attribution, presence: true
+  validates :credit_line, presence: true
 end
